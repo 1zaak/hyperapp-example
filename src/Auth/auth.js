793228b91cf9701f657f9e078ccf97d5bf0a2448
izaak <izaak@izaak.my>
2017-12-04 02:21:29 +0800

    Remove cognito libs (for now), adding hyperapp router dev branch (working 75%), added auth0 helper functions. TODO: Callback from successful auth0 to store user data into localStorage (use router)

--- src/Auth/auth.js
+++ src/Auth/auth.js
@@ -1,5 +1,6 @@
-import auth0 from 'auth0-js';
-import config from './config';
+import auth0 from 'auth0-js'
+import config from './config'
+import history from './history'
 
 const auth = new auth0.WebAuth(config);
 
@@ -12,9 +13,36 @@ export function handleAuthentication() {
         if (authResult && authResult.accessToken && authResult.idToken) {
           this.setSession(authResult);
           history.replace('/home');
+          console.log('history', history)
         } else if (err) {
           history.replace('/home');
-          console.log(err);
+          console.log('err', err);
         }
     });
 }
+
+export function setSession(authResult) {
+    // Set the time that the access token will expire at
+    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  export function logout() {
+    // Clear access token and ID token from local storage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // navigate to the home route
+    history.replace('/home');
+  }
+
+  export function isAuthenticated() {
+    // Check whether the current time is past the 
+    // access token's expiry time
+    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
+  }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import { h, app } from 'hyperapp'
-import { request } from 'graphql-request'
-import {Config, CognitoIdentityCredentials} from 'aws-sdk';
-import {
-  CognitoUserPool,
-  CognitoUserAttribute
-} from 'amazon-cognito-identity-js';
-import appConfig from "./config";
-import { login, handleAuthentication } from "./Auth/auth"
+import { h, app } from "hyperapp"
+import { location, Switch, Route, Link } from "@hyperapp/router"
+import { request } from "graphql-request"
+import { login, handleAuthentication, setSession, logout, isAuthenticated } from "./Auth/auth"
 
 /** @jsx h */
 
@@ -20,40 +15,60 @@ const query = `{
   }
 }`
 
+// Routing init
+const helloView = () =>
+<div>
+  <h2>Helllllloooooo</h2>
+</div>
+
 // hyperapp init
 app({
   state: {
     count: 0,
-    products: []
+    products: [],
+    loggedIn: false,
   },
-  view: (state, actions) =>
-    <main>
-      <h1>{state.count}</h1>    
-      <button 
-        onclick={actions.down} 
-        disabled={state.count <= 0}>ー</button>
-      <button onclick={actions.up}>＋</button>
-    <button onclick={actions.reset}>Reset</button>
-    <button onclick={actions.addProduct}>Add DJI Spark</button>
-    </main>,
   actions: {
     down: state => {
-      console.log('down clicked')
+      console.log("down clicked", isAuthenticated)
       
-      request('https://api.graph.cool/simple/v1/MobcutAlpha', query).then(data => console.log(data))
+      request("https://api.graph.cool/simple/v1/MobcutAlpha", query).then(data => console.log(data))
       return { count: state.count - 1 }
     },
     up: state => { 
-      console.log('up clicked', login)
-      login()
+      console.log("up clicked")
+      
       return { count: state.count + 1 }
     },
     addProduct: state => {
-        console.log('products', state.products)
+        console.log("products", state.products)
     
         return { products: state.products.concat({ name: "DJI Spark", price: 200000})}
     },
-    reset: state => ({ count: 0})
+    reset: state => ({ count: 0}),
+    login: state => {
+      login()
+    },
+    logout: state => {
+      logout()
+    }
+  },
+  view: (state, actions) =>
+  <main oncreate={ console.log('Oncreate!', history)}>  
+    <h1>{state.count}</h1>    
+    <button 
+      onclick={actions.down} 
+      disabled={state.count <= 0}>ー</button>
+    <button onclick={actions.up}>＋</button>
+    <button onclick={actions.reset}>Reset</button>
+    <button onclick={actions.addProduct}>Add DJI Spark</button>
     
-  }
+    <button onclick={actions.login}>Login</button>
+    <button onclick={actions.logout}>Logout</button>
+       
+    <Link to="/hello">Hello Page</Link>
+
+    <Route path="/hello" view={helloView} />
+    
+  </main>
 })
